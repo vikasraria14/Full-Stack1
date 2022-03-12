12fe9bcc97bc655068c7d6f22a44fb7e0e487f06
vikasraria14 <38049874+vikasraria14@users.noreply.github.com>
2022-03-12 11:58:18 +0530

    Exercise 2.10- But search field doesn't get updated when we click on submit button

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -10,53 +10,16 @@ const App = () => {
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+ 
   const [namesToShow, setNamesToShow]=useState(persons)
   const [searchKeys,setSearchKeys]=useState('')
 
 
 
-  const submitFunction=(event)=>
-  {
-    event.preventDefault();
-    //console.log("Hello")
-    const newNameObj={
-      name:newName,
-      number:newNumber,
-      id: persons.length+1
-    }
-    const x= persons.findIndex(person=>person.name===newNameObj.name)
-    console.log(x)
-    if(x!==-1)
-    {
-      alert(`${newNameObj.name} is already added to the phonebook`)
-    }
-    else
-    {
-      setPersons(persons.concat(newNameObj))
-      setNamesToShow(persons.concat(newNameObj))
-    }
-    setNewName('');
-    setNewNumber('');
-    setSearchKeys('');
-    
-  }
+  
   
 
-  const handleNameChange=(event)=>
-  {
-   // console.log(event.target.value);
-    setNewName(event.target.value)
-  }
-
-
-  const handleNumberChange=(event)=>
-  {
-  //  console.log(event.target.value);
-    setNewNumber(event.target.value)
-  }
-
+  
 
   const handleSearchKeys=(event)=>
   {
@@ -72,8 +35,7 @@ const App = () => {
       <h1>Phonebook</h1>
       <Filter searchKeys={searchKeys} handleSearchKeys={handleSearchKeys} />
       <h2>Add a new</h2>
-      <PersonForm newName={newName} handleNameChange={handleNameChange} 
-      newNumber={newNumber} handleNumberChange={handleNumberChange} submitFunction={submitFunction} />
+      <PersonForm persons={persons}  setPersons={setPersons} setNamesToShow={setNamesToShow} />
       <h2>Numbers</h2>
       
       {namesToShow.map(person=><Persons key={person.id} person={person}/>)}
--- Part 2/phonebook/src/PersonForm.js
+++ Part 2/phonebook/src/PersonForm.js
@@ -1,5 +1,48 @@
-const PersonForm=({newName,handleNameChange,newNumber,handleNumberChange,submitFunction})=>
+import { useState } from "react"
+const PersonForm=({persons,setPersons,setNamesToShow})=>
 {
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
+
+    const handleNameChange=(event)=>
+  {
+   // console.log(event.target.value);
+    setNewName(event.target.value)
+  }
+
+
+  const handleNumberChange=(event)=>
+  {
+  //  console.log(event.target.value);
+    setNewNumber(event.target.value)
+  }
+
+  const submitFunction=(event)=>
+  {
+    event.preventDefault();
+    //console.log("Hello")
+    const newNameObj={
+      name:newName,
+      number:newNumber,
+      id: persons.length+1
+    }
+    const x= persons.findIndex(person=>person.name===newNameObj.name)
+    console.log(x)
+    if(x!==-1)
+    {
+      alert(`${newNameObj.name} is already added to the phonebook`)
+    }
+    else
+    {
+      setPersons(persons.concat(newNameObj))
+      setNamesToShow(persons.concat(newNameObj))
+    }
+    setNewName('');
+    setNewNumber('');
+   // setSearchKeys('');
+    
+  }
+
     return(
         <form>
         <div>
