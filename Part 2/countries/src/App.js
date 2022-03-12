b2e3500a2733d847f2c7f390b22b5fdd7517f8f0
vikasraria14 <38049874+vikasraria14@users.noreply.github.com>
2022-03-12 20:54:49 +0530

    Languages has some unwanted text, which i will try to solve using showLang function

--- Part 2/countries/src/App.js
+++ Part 2/countries/src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const url='https://restcountries.com/v3.1/all';
+  const [countries,setCountries]=useState([])
+  const [countriesToShow,setCountriesToShow]=useState([]);
+  const [searchField,setSearchField]=useState('')
+  useEffect(
+    ()=>{
+      axios.get(url)
+      .then(response=>{
+        setCountries(response.data)
+       // console.log(response.data)
+        setCountriesToShow(response.data)
+        
+      });
+    }
+    ,[])
+    const showLang=(languages)=>
+    {
+      for(let key in languages)
+      {
+        console.log(languages[key])
+      }
+    }
+    const handleNameChange=event=>{
+      const x=event.target.value;
+      setSearchField(x)
+      
+      const updatedCountries= countries.filter(country=>country.name.common.toLowerCase().includes(x.toLowerCase()))
+      setCountriesToShow(updatedCountries)
+      
+    
+    }
+    let langs;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <form>
+        Find Countries: <input value={searchField} onChange={handleNameChange}/>
+      </form>
+      {
+        countriesToShow.length>10?
+        'Too many matches, try another filter':        
+        (countriesToShow.length===1?
+        
+        <div>
+          <h1>{countriesToShow[0].name.common}</h1>
+          <p>Capital: {countriesToShow[0].capital}</p>
+          <p>Area: {countriesToShow[0].area}</p>
+          <h2>Languages</h2>
+          <ul>
+            
+            
+            {langs=Object.keys(countriesToShow[0].languages)}
+            {console.log('langs' ,langs)}
+            {langs.map((lang)=><li key={lang}>{countriesToShow[0].languages[lang]}</li>)}
+            
+          </ul>
+          <img src=
+          {countriesToShow[0].flags.png} alt='Flag'
+          />
+        </div>
+        :
+         countriesToShow.map(country=><p key={country.name.official}>{country.name.common}</p>))        
+          
+          
+        
+        
+      }
     </div>
   );
 }
